4b73d62597f0ebf959e368ef50a54a1449857d11
Dhanish <dhanish.b.gajjar@gmail.com>
2017-06-06 17:35:00 +0100

    Second Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+let data = [
+	{ id: 1, "parent": null, "name": "Electronics" },
+	{ id: 2, "parent": 1, "name": "Television" },
+	{ id: 3, "parent": 1, "name": "Portable Electronics" },
+	{ id: 4, "parent": 2, "name": "Tube" },
+	{ id: 5, "parent": 2, "name": "LCD" },
+	{ id: 6, "parent": 2, "name": "Plasma" },
+	{ id: 7, "parent": 3, "name": "MP3 Players" },
+	{ id: 8, "parent": 3, "name": "CD Players" },
+	{ id: 9, "parent": 3, "name": "2 Way Radios" },
+	{ id: 10, "parent": 7, "name": "Flash" }
+]
+
+let render = "";
+
+function getChildren(array, parent_index) {
+	let children = []
+	for(let i = 0; i < array.length; i++) {
+		let node = array[i]
+		if(node["id"] === parent_index) {
+			render += "<ul>" + node["name"]
+			continue
+		}
+		if(node["parent"] === parent_index) {
+			// render += "<li>" + children[node["name"]]
+			children[node["name"]] = {
+				"id" : node["id"], "name" : node["name"], "children" : getChildren(array, node["id"])
+			}
+			// render += "</li>"
+		}
+	}
+	render += "</ul>"
+	return children
+}
+
+let output = getChildren(data, null)
+let container = document.getElementById("output")
+container.innerHTML += render;
+console.log(output)
